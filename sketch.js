aca0e5a9576389760317b666b6119f665431f4ee
steinikarls <steinikarls@live.com>
2017-12-03 05:21:37 +0000

    finished
    finished

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 "use strict";
+new p5();
+
 function make2DArray(cols, rows) {
 	var arr = new Array(cols);
 	for (var i = 0; i < arr.length; i++) {
@@ -8,33 +10,79 @@ function make2DArray(cols, rows) {
 }
 
 
+
+
 var grid;
 var cols;
 var rows;
 var w = 20;
+var totalBees = 50;
 
 function setup() {
-  createCanvas(401, 401);
-  cols = floor(width / w);
-  rows = floor(height / w);
-  window.console.log(rows);
-  grid = make2DArray(cols, rows);
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j] = new Cell(i, j, w);
-    }
+	createCanvas(401, 401);
+	cols = floor(width / w);
+	rows = floor(height / w);
+	grid = make2DArray(cols, rows);
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			grid[i][j] = new Cell(i, j, w);
+		}
+	}
+
+
+	var options = [];
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			options.push([i,j]);
+		}
+	}
+
+
+	
+  for (var n = 0; n < totalBees; n++) {
+    var index = floor(random(options.length));
+    var choice = options[index];
+    var i = choice[0];
+    var j = choice[1];
+    // Deletes that spot so it's no longer an option
+    options.splice(index, 1);
+    grid[i][j].bee = true;
   }
+
+
+
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			grid[i][j].countBees();
+		}
+	}
+	
 }
+
+function gameOver() {
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			grid[i][j].revealed = true;
+		}
+	}
+}
+
+
 
 function mousePressed() {
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
-			if(grid[i][j].contains(mouseX, mouseY)){
+			if (grid[i][j].contains(mouseX, mouseY)) {
 				grid[i][j].reveal();
+			
+				if (grid[i][j].bee) {
+					gameOver();
+				}
 			}
 		}
 	}
 }
+
 
 
 
@@ -46,5 +94,6 @@ function draw() {
 		}
 	}
 }
+
 
 
